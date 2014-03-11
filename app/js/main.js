2eaa973c614e9c2780d18b522e985cdb7ee3b87a
kepennar <kevin.pennarun@gmail.com>
2014-03-11 15:06:02 +0100

    Extract js from html to main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
+(function() {
+    var lang = 'fr';
+
+	$(".templatedElement").each(function(i, elem) {
+		var $elem = $(elem);
+		var templateId = $elem.data('template-id');
+
+		templateLoader.getTemplate(templateId).then(function(template) {
+	    	templateLoader.getResources(templateId, lang).then(function(context) {
+	    		$elem.html(template(context));	
+	    	});
+		});
+	});
+	skrollr.init({
+	    mobileCheck: function() {
+	        return false;
+	    },
+	    forceHeight: false
+	});
+
+}());
 
